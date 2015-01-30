8409fbd97afe69597791358ec23dfa1f90011cf6
root <root@localhost>
2015-01-30 01:03:35 +0100

    Update docs

---
+++ target/doc/implementors/core/iter/trait.FromIterator.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['ixlist'] = ["<a class='stability Stable' title='Stable'></a>impl&lt;'a, T&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/iter/trait.FromIterator.html' title='core::iter::FromIterator'>FromIterator</a>&lt;T&gt; for <a class='struct' href='ixlist/struct.List.html' title='ixlist::List'>List</a>&lt;T&gt;",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
