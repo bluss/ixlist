0b78098fd7a4b893bce0f33258becdd34635da20
bluss <bluss@users.noreply.github.com>
2017-09-14 21:09:41 +0200

    FEAT: Update benchmark code to use stable

--- benches/bench1.rs
+++ benches/bench1.rs
@@ -1,37 +1,42 @@
-#![feature(test)]
 extern crate ixlist;
-extern crate test;
 extern crate rand;
+#[macro_use] extern crate bencher;
+
+use bencher::Bencher;
 
 use rand::{Rng};
+use rand::XorShiftRng;
 use std::collections::{LinkedList, VecDeque};
 use ixlist::{
     List,
 };
+use bencher::black_box;
+
+// fn id<T>(t: T) -> T { t }
+
+// reproducible rng
+fn repro_rng() -> XorShiftRng { XorShiftRng::new_unseeded() }
 
 
-
-#[bench]
-fn push_front_dlist(b: &mut test::Bencher)
+fn push_front_dlist(b: &mut Bencher)
 {
     b.iter(|| {
         let mut dl = LinkedList::new();
         let n = 1000;
         for _ in (0..n) {
-            dl.push_front(test::black_box(1));
+            dl.push_front(black_box(1));
         }
         dl
     })
 }
 
-#[bench]
-fn push_front_ringbuf(b: &mut test::Bencher)
+fn push_front_ringbuf(b: &mut Bencher)
 {
     b.iter(|| {
         let mut l = VecDeque::new();
         let n = 1000;
         for _ in (0..n) {
-            l.push_front(test::black_box(1));
+            l.push_front(black_box(1));
         }
         l
     })
@@ -46,98 +51,103 @@ fn push_front_ringbuf_cap(b: &mut test::Bencher)
         let N = 1000;
         let mut l = VecDeque::with_capacity(N);
         for _ in (0..N) {
-            l.push_front(test::black_box(1));
+            l.push_front(black_box(1));
         }
         l
     })
 }
 */
 
-#[bench]
-fn push_front_list(b: &mut test::Bencher)
+fn push_front_list(b: &mut Bencher)
 {
     b.iter(|| {
         let mut l = List::new();
         let n = 1000;
         for _ in (0..n) {
-            l.push_front(test::black_box(1));
+            l.push_front(black_box(1));
         }
         l
     })
 }
 
-#[bench]
-fn push_front_list_cap(b: &mut test::Bencher)
+fn push_front_list_cap(b: &mut Bencher)
 {
     b.iter(|| {
         let n = 1000;
         let mut l = List::with_capacity(n);
         for _ in (0..n) {
-            l.push_front(test::black_box(1));
+            l.push_front(black_box(1));
         }
         l
     })
 }
 
-#[bench]
-fn iterate_dlist(b: &mut test::Bencher)
+fn iterate_dlist(b: &mut Bencher)
 {
     let mut dl = LinkedList::new();
     let n = 1000;
-    let mut rng = rand::weak_rng();
+    let mut rng = repro_rng();
     for _ in (0..n) {
         if rng.gen() {
-            dl.push_front(test::black_box(1));
+            dl.push_front(black_box(1));
         } else {
-            dl.push_back(test::black_box(1));
+            dl.push_back(black_box(1));
         }
     }
     b.iter(|| {
         for elt in dl.iter() {
-            test::black_box(elt);
+            black_box(elt);
         }
     })
 }
 
-#[bench]
-fn iterate_ringbuf(b: &mut test::Bencher)
+fn iterate_ringbuf(b: &mut Bencher)
 {
     let mut dl = VecDeque::new();
     let n = 1000;
-    let mut rng = rand::weak_rng();
+    let mut rng = repro_rng();
     // scramble a bit so we get a random access iteration
     for _ in (0..n) {
         if rng.gen() {
-            dl.push_front(test::black_box(1));
+            dl.push_front(black_box(1));
         } else {
-            dl.push_back(test::black_box(1));
+            dl.push_back(black_box(1));
         }
     }
     b.iter(|| {
         for elt in dl.iter() {
-            test::black_box(elt);
+            black_box(elt);
         }
     })
 }
 
 
-#[bench]
-fn iterate_list(b: &mut test::Bencher)
+fn iterate_list(b: &mut Bencher)
 {
     let mut dl = List::new();
     let n = 1000;
-    let mut rng = rand::weak_rng();
+    let mut rng = repro_rng();
     // scramble a bit so we get a random access iteration
     for _ in (0..n) {
         if rng.gen() {
-            dl.push_front(test::black_box(1));
+            dl.push_front(black_box(1));
         } else {
-            dl.push_back(test::black_box(1));
+            dl.push_back(black_box(1));
         }
     }
     b.iter(|| {
         for elt in dl.iter() {
-            test::black_box(elt);
+            black_box(elt);
         }
     })
 }
+
+benchmark_group!(benches,
+                 push_front_dlist,
+                 push_front_ringbuf,
+                 push_front_list,
+                 push_front_list_cap,
+                 iterate_dlist,
+                 iterate_ringbuf,
+                 iterate_list);
+benchmark_main!(benches);
